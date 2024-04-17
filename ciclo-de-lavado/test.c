3a054eb70c45a2e76cd9e3c6035c9ddbcc107fa4
Ivan Gordillo <ivan.gordillo@cua.uam.mx>
2024-04-17 08:05:25 -0600

    fix include

--- ciclo-de-lavado/test.c
+++ ciclo-de-lavado/test.c
@@ -1,7 +1,7 @@
 #include    <stdio.h>
 #include    "pico/stdlib.h"
 #include    "hardware/gpio.h"
-#include    "cicloLavado.c"
+#include    "cicloLavado.h"
 
 #define BUTTON_CICLO 7
 #define LED_SUAVE 4
