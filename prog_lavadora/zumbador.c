fd4e4f449fddee205aadc0eca4619a03cdeaf70a
Joshua Soria <joshua.jacuinde@cua.uam.mx>
2024-04-17 19:41:53 -0600

    prueba con botones

--- prog_lavadora/zumbador.c
+++ prog_lavadora/zumbador.c
@@ -3,30 +3,42 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-#define Zumbador 2
+#define Zumbador 16
+#define boton 0
 
 int sonidos[8] = {
-    1, 0, 0, 0, 1, 1, 0, 1
+    1, 1, 0, 1, 0, 1, 0, 0
 };
 
 int tiempos[8] = {
-  30,30,10,30,30,20,10,10
+  30, 30, 10, 30, 10, 30, 10, 10
 };
 
+
 int main(){
-    //stdio_init_all();
+    stdio_init_all();
     gpio_init(Zumbador);
     gpio_set_dir(Zumbador, GPIO_OUT);
+
+    gpio_init(boton);
+    gpio_set_dir(boton, GPIO_IN);
+    gpio_pull_up(boton);
     
     while (true)
     {
-        for (int i = 0; i < 8; i++)
+        if (gpio_get(boton))
         {
-            int32_t mask = sonidos[i] << Zumbador;
-            gpio_set_mask(mask);
-            sleep_ms(tiempos[i]);
-            gpio_clr_mask(mask);
+            for (int i = 0; i < 8; i++)
+            {
+                int32_t mask = sonidos[i] << Zumbador;
+                gpio_set_mask(mask);
+                sleep_ms(tiempos[i]);
+                gpio_clr_mask(mask);
+            }
+            sleep_ms(500);
         }
+        
+        
     }
          
 
