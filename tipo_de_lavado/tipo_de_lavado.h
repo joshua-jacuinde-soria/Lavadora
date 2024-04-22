629aa73789716e6bba3b510cd48cadcc9fb1a750
Alan David Benítez García <david.51699@gmail.com>
2024-04-22 08:24:14 -0600

    Declaracion de los botones en el archivo head

--- tipo_de_lavado/tipo_de_lavado.h
+++ tipo_de_lavado/tipo_de_lavado.h
@@ -1,1 +1,9 @@
 #include "hardware/gpio.h"
+
+/* Aquí se definen constantes para los números de pin de 
+los GPIOs a los que están conectados los LEDs y el botón. */ 
+
+const uint LED_lavar = 8;           // Led para lavar
+const uint LED_enjuagar = 9;        // Led para enjuagar
+const uint LED_centrifugar = 10;    // Led para centrifugar
+const uint BOTON = 11;              // Botón de acción
