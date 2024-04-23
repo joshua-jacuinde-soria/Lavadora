6148f10126372bea4f3300cbc07927ce5c6fc449
Alan David Benítez García <david.51699@gmail.com>
2024-04-23 11:25:18 -0600

    Se añaden en el archivo pruebas.c los puertos GPIO para trabajar

--- tipo_de_lavado/pruebas_tipo_lavado.c
+++ tipo_de_lavado/pruebas_tipo_lavado.c
@@ -7,3 +7,18 @@ const uint LED_lavar = 8;           // Led para lavar
 const uint LED_enjuagar = 9;        // Led para enjuagar
 const uint LED_centrifugar = 10;    // Led para centrifugar
 const uint BOTON = 11;              // Botón de acción
+
+int main (){
+
+    stdio_init_all();
+
+    /* Cada llamada a gpio_init() inicializa un pin GPIO. 
+    Esta función debe ser llamada en cada pin antes de 
+    configurarlo para entrada o salida.*/
+
+    gpio_init(LED_lavar);
+    gpio_init(LED_enjuagar);
+    gpio_init(LED_centrifugar);
+    gpio_init(BOTON);
+
+}
