ed2283a9a5713890cb4cc66567ef750265b4b8bd
Alan David Benítez García <david.51699@gmail.com>
2024-04-23 11:45:36 -0600

    pruebas_tipo_lavado.c : Se corrigen errores de compilación en la definición de los leds

--- tipo_de_lavado/pruebas_tipo_lavado.c
+++ tipo_de_lavado/pruebas_tipo_lavado.c
@@ -3,10 +3,10 @@
 #include "hardware/gpio.h"
 #include "tipo_de_lavado.h"
 
-const uint LED_lavar = 8;           // Led para lavar
-const uint LED_enjuagar = 9;        // Led para enjuagar
-const uint LED_centrifugar = 10;    // Led para centrifugar
-const uint BOTON = 11;              // Botón de acción
+#define LED_lavar  8           // Led para lavar
+#define LED_enjuagar  9        // Led para enjuagar
+#define LED_centrifugar  10    // Led para centrifugar
+#define BOTON  11              // Botón de acción
 
 int main (){
 
