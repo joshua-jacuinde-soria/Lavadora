5572ffd0e56dcca815cf98a68649daab0a716b8e
Alan David Benítez García <david.51699@gmail.com>
2024-04-23 11:17:35 -0600

    Se añaden los valores de los leds y el boton al archivo pruebas.c

--- tipo_de_lavado/pruebas_tipo_lavado.c
+++ tipo_de_lavado/pruebas_tipo_lavado.c
@@ -1,3 +1,9 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "tipo_de_lavado.h"
+
+const uint LED_lavar = 8;           // Led para lavar
+const uint LED_enjuagar = 9;        // Led para enjuagar
+const uint LED_centrifugar = 10;    // Led para centrifugar
+const uint BOTON = 11;              // Botón de acción
