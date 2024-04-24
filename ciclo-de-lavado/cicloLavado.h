345151598097679f9a732177b28a80f0a162e2c2
Ivan Gordillo <ivan.gordillo@cua.uam.mx>
2024-04-24 14:00:49 -0600

    fix logic

--- ciclo-de-lavado/cicloLavado.h
+++ ciclo-de-lavado/cicloLavado.h
@@ -2,14 +2,44 @@
 #define CICLO_LAVADO_H
 
 #include "hardware/gpio.h"
+#include "../zumbador/zumbador.h"
 
 // Definir  los pines de los LEDs
 #define LED_SUAVE 4
 #define LED_PESADO 5
 #define LED_RAPIDO 6
+#define BOTON 7
+
+int cycleStatus;
+
+void inicio_cicloLavado() {
+    // Inicializar los pines de los LEDs
+    gpio_init(LED_SUAVE);
+    gpio_init(LED_PESADO);
+    gpio_init(LED_RAPIDO);
+    // Configurar los pines de los LEDs como salida
+    gpio_set_dir(LED_SUAVE, GPIO_OUT);
+    gpio_set_dir(LED_PESADO, GPIO_OUT);
+    gpio_set_dir(LED_RAPIDO, GPIO_OUT);
+    // Inicializar el boton
+    gpio_init(BOTON);
+    gpio_set_dir(BOTON, GPIO_IN);
+    gpio_pull_up(BOTON);
+
+    // Apagar los LEDs
+    gpio_put(LED_SUAVE, 0);
+    gpio_put(LED_PESADO, 0);
+    gpio_put(LED_RAPIDO, 0);
+
+    cycleStatus = -1;
+}
+
 int cicloLavado(int cycleStatus) {
-    // Cambiar el ciclo de lavado
-    cycleStatus++;
+    // Cuando el boton sea presionado
+    if (!gpio_get(BOTON)) {
+        cycleStatus++;
+        zumbador(1);
+    }
     if (cycleStatus > 2) {
         cycleStatus = 0;
     }
@@ -17,8 +47,6 @@ int cicloLavado(int cycleStatus) {
     gpio_put(LED_SUAVE, cycleStatus == 0);
     gpio_put(LED_PESADO, cycleStatus == 1);
     gpio_put(LED_RAPIDO, cycleStatus == 2);
-
-    return cycleStatus;
 }
 
 #endif
