237a9f75509817da3cde4b224bb10e076cc90f2a
Alan David Benítez García <david.51699@gmail.com>
2024-05-26 22:17:10 -0600

    Se aañaden las prefunciones de los sensores

--- MQTT/MQTT_Final.py
+++ MQTT/MQTT_Final.py
@@ -9,7 +9,15 @@ import BlynkLib
 MQTT_SERVER = '192.168.1.74'
 MQTT_PORT = 1883
 MQTT_TOPIC_peso = 'sensors/peso'
-MQTT_TOPIC_DHT1 = 'sensors/temp'
-MQTT_TOPIC_DHT2 = 'sensors/hum'
+MQTT_TOPIC_2 = 'sensors/2'
+MQTT_TOPIC_3 = 'sensors/3'
 client = MQTTClient('Raspberrry_Pi_Pico_W', MQTT_SERVER, port=MQTT_PORT)
 
+def read_peso():
+    # Aqui va todo lo relacionado al sensor de peso
+
+def read_2():
+    # Aqui va todo lo relacionado al sensor 2
+
+def read_3():
+    # Aqui va todo lo relacionado al sensor 3
