7efda9f9b010afa17b601a967cca2c17df255dc1
Alan David Benítez García <david.51699@gmail.com>
2024-05-27 20:30:57 -0600

    Se agrega la funcion del sensor_ultrasonico

--- MQTT/MQTT_Final.py
+++ MQTT/MQTT_Final.py
@@ -1,5 +1,6 @@
 import network
 import time
+from time import sleep, sleep_us, ticks_us
 from machine import Pin, ADC
 from simple import *
 import dht
@@ -8,14 +9,42 @@ import BlynkLib
 # Configuración del broker MQTT
 MQTT_SERVER = '192.168.1.74'
 MQTT_PORT = 1883
-MQTT_TOPIC_peso = 'sensors/peso'
-MQTT_TOPIC_2 = 'sensors/2'
-MQTT_TOPIC_3 = 'sensors/3'
+#MQTT_TOPIC_peso = 'sensors/peso'
+MQTT_TOPIC_ultra_L = 'sensors/ultraL'
+MQTT_TOPIC_ultra_D = 'sensors/ultraD'
+#MQTT_TOPIC_3 = 'sensors/3'
 client = MQTTClient('Raspberrry_Pi_Pico_W', MQTT_SERVER, port=MQTT_PORT)
 
 def read_peso():
     # Aqui va todo lo relacionado al sensor de peso
     return 0
+
+def ultra():
+    trig = Pin(17, Pin.OUT)
+    echo = Pin(16, Pin.IN, Pin.PULL_DOWN)
+    trig.value(0)
+    sleep(0.1)
+    trig.value(1)
+    sleep_us(2)
+    trig.value(0)
+    while echo.value()==0:
+        pulse_start = ticks_us()
+    while echo.value()==1:
+        pulse_end = ticks_us()
+    pulse_duration = float(pulse_end) - float(pulse_start)
+    distance = pulse_duration * 17165 / 1000000
+    distance = round(distance, 0)
+    if distance < 20:
+        level = 0
+    elif distance >= 20 and distance < 100:
+        level = 1
+    elif distance >= 100 and distance < 200:
+        level = 2
+    elif distance >= 200 and distance < 300:
+        level = 3
+    else:
+        level = 4
+    return level, distance
 
 def read_2():
     # Aqui va todo lo relacionado al sensor 2
@@ -36,25 +65,28 @@ def read_and_publish():
     blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
     while True:
-        peso = read_peso()
-        print('Luz_T: ', peso)
-        client.publish(MQTT_TOPIC_peso, str(peso))
+        """peso = read_peso()
+        print('Peso: ', peso)
+        client.publish(MQTT_TOPIC_peso, str(peso))"""
 
-        Sensor_2 = read_2()
-        print('DHT1: ', Sensor_2)
-        client.publish(MQTT_TOPIC_2, str(Sensor_2))
+        level, distance = ultra()
+        print('Nivel: ', level)
+        client.publish(MQTT_TOPIC_ultra_L, str(level))
+
+        print('Distancia: ', distance)
+        client.publish(MQTT_TOPIC_ultra_D, str(distance))
         
-        Sensor_3 = read_3()
+        """Sensor_3 = read_3()
         print('DHT2: ', Sensor_3)
-        client.publish(MQTT_TOPIC_3, str(Sensor_3))
+        client.publish(MQTT_TOPIC_3, str(Sensor_3))"""
         
         # Tiempo de espera
         time.sleep(5)
 
         # Send sensor data to Blynk
-        blynk.virtual_write(0, peso)  # virtual pin 0 for peso
-        blynk.virtual_write(1, Sensor_2)    # virtual pin 2 for Sensor 2
-        blynk.virtual_write(2, Sensor_3)   # virtual pin 3 for Sensor 3
+        #blynk.virtual_write(0, peso)  # virtual pin 0 for peso
+        blynk.virtual_write(1, level)    # virtual pin 1 for nivel
+        blynk.virtual_write(2, distance)   # virtual pin 2 for distancia
 
         # Run Blynk
         blynk.run()
