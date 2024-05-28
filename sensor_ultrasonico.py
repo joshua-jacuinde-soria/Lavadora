fab3a043d0b1dfc4a1a7db1a0ffea2c3e1ed75d6
Joshua Soria <joshua.jacuinde@cua.uam.mx>
2024-05-27 19:39:52 -0600

    pruebas de función

--- sensor_ultrasonico.py
+++ sensor_ultrasonico.py
@@ -1,18 +1,18 @@
 from machine import Pin
-import time
+from time import sleep, sleep_us, ticks_us
 
 trig = Pin(17, Pin.OUT)
 echo = Pin(16, Pin.IN, Pin.PULL_DOWN)
 def nivel():
      trig.value(0)
-     time.sleep(0.1)
+     sleep(0.1)
      trig.value(1)
-     time.sleep_us(2)
+     sleep_us(2)
      trig.value(0)
      while echo.value()==0:
-          pulse_start = time.ticks_us()
+          pulse_start = ticks_us()
      while echo.value()==1:
-          pulse_end = time.ticks_us()
+          pulse_end = ticks_us()
      pulse_duration = float(pulse_end) - float(pulse_start)
      distance = pulse_duration * 17165 / 1000000
      distance = round(distance, 0)
@@ -29,4 +29,17 @@ def nivel():
      return level
      
 def main():
-     nivel()
+     while True:
+          level = nivel()
+          if level == 0:
+               mensaje = 'alerta muy lleno'
+          elif level == 1:
+               mensaje = 'alto'
+          elif level == 2:
+               mensaje = 'medio'
+          elif level == 3:
+               mensaje = 'bajo'
+          else:
+               mensaje = 'minimo'
+          print('Nivel de agua: ', mensaje)
+          sleep(1)
