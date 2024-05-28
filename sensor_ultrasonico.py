2f4af3b49dbc3266c5d04af3539ba0d791a20e78
Joshua Soria <joshua.jacuinde@cua.uam.mx>
2024-05-27 19:38:33 -0600

    funcion con valores

--- sensor_ultrasonico.py
+++ sensor_ultrasonico.py
@@ -16,6 +16,17 @@ def nivel():
      pulse_duration = float(pulse_end) - float(pulse_start)
      distance = pulse_duration * 17165 / 1000000
      distance = round(distance, 0)
+     if distance < 20:
+          level = 0
+     elif distance >= 20 and distance < 100:
+          level = 1
+     elif distance >= 100 and distance < 200:
+          level = 2
+     elif distance >= 200 and distance < 300:
+          level = 3
+     else:
+          level = 4
+     return level
      
 def main():
      nivel()
